1dd688986d1b347109d2b68a706455b31dc98ee6
Boris Blokland <boris.blokland@alten.nl>
2024-07-16 09:25:50 +0200

    use normal parse

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -4,27 +4,25 @@ from behave import given, when, then, use_step_matcher
 from course import Course
 from datetime import datetime
 
-use_step_matcher("cfparse")
-
 # GIVEN STEPS
 
-@given(u'there is a course with {count:d} registrations')
+@given("there is a course with {count:d} registrations")
 def course_with_registrations(context, count):
     context.course = Course()
     context.course.registrations = count
     context.current_date = datetime.now
 
-@given(u'I enter {name}')
+@given("I enter {name}")
 def step_impl(context, name):
     context.name = name
 
 
-@when(u'I register')
+@when("I register")
 def step_impl(context):
     context.course.register(context.name)
 
 
-@then(u'the course has {count:d} registration')
+@then("the course has {count:d} registration")
 def step_impl(context, count):
     assert context.course.registrations == count
 
